e7fdf2a4ff8745557e5254a943050aa6778e4dba
Nuno Frade <nfrade.tribal@gmail.com>
2020-03-17 10:31:51 +0100

    Adds falling back to murmurhash-native if cannot load prebuilt

--- index.js
+++ index.js
@@ -12,9 +12,24 @@ module.exports = (() => {
 
     return murmur;
   } catch (e) {
-    throw new Error(
-      "This version of murmur is not compatible with your Node.js build:\n\n" +
-        e.toString()
+    const fs = require("fs");
+    const pathResolve = require("path").resolve;
+    const { execSync } = require("child_process");
+    let murmur;
+    console.log(
+      "Cannot use prebuilt Murmur version. Falling back to murmurhash-native."
     );
+    // check for murmurhash-native without adding it to the require cache
+    module.paths.forEach(path => {
+      if (fs.existsSync(pathResolve(path, "murmurhash-native")) === true) {
+        murmur = require("murmurhash-native");
+      }
+    });
+    if (!murmur) {
+      console.log("Installing murmurhash-native");
+      execSync("npm install murmurhash-native");
+      murmur = require("murmurhash-native");
+    }
+    return murmur;
   }
 })();

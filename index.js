48f4a650990cca297229b25446c6fb2ddb117599
youzi <youri@saulx.com>
2021-01-13 15:32:28 +0100

    add browser shim

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 "use strict";
 
 module.exports = (() => {
-  try {
-    const murmur = require("./builds/" +
-      process.platform +
-      "_" +
-      process.arch +
-      "_" +
-      process.versions.modules +
-      ".node");
+  if (typeof window === "undefined") {
+    try {
+      const murmur = require("./builds/" +
+        process.platform +
+        "_" +
+        process.arch +
+        "_" +
+        process.versions.modules +
+        ".node");
 
-    return murmur;
-  } catch (e) {
-    const fs = require("fs");
-    const pathResolve = require("path").resolve;
-    const { execSync } = require("child_process");
-    let murmur;
-    console.log(
-      "Cannot use prebuilt Murmur version. Falling back to murmurhash-native."
-    );
-    // check for murmurhash-native without adding it to the require cache
-    module.paths.forEach(path => {
-      if (fs.existsSync(pathResolve(path, "murmurhash-native")) === true) {
+      return murmur;
+    } catch (e) {
+      const fs = require("fs");
+      const pathResolve = require("path").resolve;
+      const { execSync } = require("child_process");
+      let murmur;
+      console.log(
+        "Cannot use prebuilt Murmur version. Falling back to murmurhash-native."
+      );
+      // check for murmurhash-native without adding it to the require cache
+      module.paths.forEach((path) => {
+        if (fs.existsSync(pathResolve(path, "murmurhash-native")) === true) {
+          murmur = require("murmurhash-native");
+        }
+      });
+      if (!murmur) {
+        console.log("Installing murmurhash-native");
+        execSync("npm install murmurhash-native");
         murmur = require("murmurhash-native");
       }
-    });
-    if (!murmur) {
-      console.log("Installing murmurhash-native");
-      execSync("npm install murmurhash-native");
-      murmur = require("murmurhash-native");
+      return murmur;
     }
-    return murmur;
+  } else {
+    return () => {};
   }
 })();

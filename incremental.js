598debc6fea872e002e59552598ad532e2e86921
Jim de Beer <beerdejim@gmail.com>
2021-11-16 11:36:04 +0100

    build for node 16

--- incremental.js
+++ incremental.js
@@ -1,13 +1,20 @@
 "use strict";
 
 const getBuild = () => {
-  const murmur = require("./builds/incremental/" +
-    process.platform +
-    "_" +
-    process.arch +
-    "_" +
-    process.versions.modules +
-    ".node");
+  let murmur;
+  try {
+    console.info(process.platform, process.arch, process.versions.modules);
+    murmur = require("./builds/incremental" +
+      process.platform +
+      "_" +
+      process.arch +
+      "_" +
+      process.versions.modules +
+      ".node");
+  } catch (err) {
+    // murmur = require("murmurhash-native/incremental");
+    // console.log(murmur);
+  }
 
   return murmur;
 };
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 "use strict";
 
 const getBuild = () => {
-  const murmur = require("./builds/" +
-    process.platform +
-    "_" +
-    process.arch +
-    "_" +
-    process.versions.modules +
-    ".node");
+  let murmur;
+  try {
+    murmur = require("./builds/" +
+      process.platform +
+      "_" +
+      process.arch +
+      "_" +
+      process.versions.modules +
+      ".node");
+  } catch (err) {
+    // console.info("cannot find prebuid", err);
+    // murmur = require("murmurhash-native");
+    // console.log(murmur);
+  }
 
   return murmur;
 };
